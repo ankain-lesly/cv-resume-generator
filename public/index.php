e4df3df0e5dd799c2924e5c98f8534e24d424109
Ankain Lesly <leeleslyank@gmail.com>
2023-07-11 12:39:47 -0700

    API Routes and Requests setup

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+declare(strict_types=1);
+header('Accept-Control-Allow-Origin:*');
+header("Content-Type: application/json; charset=UTF-8");
 
 /** User: Dev Lee ... */
 require_once __DIR__ . "/../vendor/autoload.php";
@@ -11,31 +14,36 @@ use App\controllers\AuthController;
 $router = new Router(__DIR__);
 
 // Database Configurations
-$DB_Config = [
-  'host' => 'localhost',
-  'user' => 'root',
-  'password' => '',
-  'name' => 'ken-car-web',
-  //'name' => 'db_demo_blog',
-];
+
+$DB_Config['host'] 		= 'localhost';
+$DB_Config['user'] 		= 'root';
+$DB_Config['password'] 	= '';
+$DB_Config['name'] 		= 'db_demo_blog';
 
 DBModel::SetDatabaseDetails($DB_Config);
 // Router Configurations
+
 $router->config("views", "layouts/main", "_404");
 
-// $router->interceptRequest();
+$router->interceptRequest();
 
+// Regular Views
 $router->get("/", [SiteController::class, 'index']);
 $router->get("/dashboard", "@dashboard");
 $router->get("/module", "Module");
 
-// $router->get("/create/post", [SiteController::class, 'createPost']);
-// $router->post("/create/post", [SiteController::class, 'createPost']);
+//API
+$router->get("/api/test/", function($req, $res) {
+	$res->json(["status" => "success", "user" => "Dev Lee"]);
+});
+
+$router->get("/create/post", [SiteController::class, 'createPost']);
+$router->post("/api/create/post", [SiteController::class, 'createPost']);
 
 $router->get("/login", [AuthController::class, 'login']);
-$router->post("/login", [AuthController::class, 'login']);
+$router->post("/api/auth/login", [AuthController::class, 'login']);
 
 $router->get("/register", [AuthController::class, 'register']);
-$router->post("/register", [AuthController::class, 'register']);
+$router->post("/api/auth/register", [AuthController::class, 'register']);
 
 $router->resolve();
