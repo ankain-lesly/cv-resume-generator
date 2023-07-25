18b7dd51a9dd0e08ac86c5867342c15cfffdafef
Ankain Lesly <leeleslyank@gmail.com>
2023-07-24 20:48:58 -0700

    Introducing middle wares

--- Middlewares/AuthMiddleware.php
+++ Middlewares/AuthMiddleware.php
@@ -5,22 +5,4 @@ namespace App\Middlewares;
 
 class AuthMiddleware
 {
-  public function __construct()
-  {
-    /**
-     * Introducing middle wares
-     *
-     * Middleware => AuthMiddleware
-     *  ::isUser(token);
-     *  ::isAdmin(role);
-     *
-     */
-  }
-
-  public function isUser()
-  {
-  }
-  public function isAdmin()
-  {
-  }
 }
