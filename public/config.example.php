8852765f03c8fc121c60b74c99c1cf63ba246798
Ankain Lesly <leeleslyank@gmail.com>
2023-08-04 06:04:33 -0700

    Setting up changes

---
+++ public/config.example.php
@@ -0,0 +1,13 @@
+<?php
+// Custom Assets
+define("ROOT_URL", "https://www.website.com");
+define("MAIN_EMAIL", "email@server.com");
+
+// Host Server
+
+
+// Local Database
+define("DB_HOST", "host");
+define("DB_USERNAME", "user");
+define("DB_PASSWORD", "password");
+define("DB_NAME", "databasename");
--- public/config.php
+++ public/config.php
@@ -4,15 +4,13 @@ define("ROOT_URL", "https://www.letech-cg.com");
 define("MAIN_EMAIL", "support@letech-cg.com");
 
 // Host Server
-define("DB_HOST", "localhost");
-define("DB_USERNAME", "u977614154_lee");
-define("DB_PASSWORD", "Db_resumes_123");
-define("DB_NAME", "u977614154_Db_resumes");
-// define("DB_NAME", "db_cv_generator");
+// define("DB_HOST", "localhost");
+// define("DB_USERNAME", "u977614154_lee");
+// define("DB_PASSWORD", "Db_resumes_123");
+// define("DB_NAME", "u977614154_Db_resumes"); 
 
 // Local Database
-// define("DB_HOST", "localhost");
-// define("DB_USERNAME", "root");
-// define("DB_PASSWORD", "");
-// define("DB_NAME", "db_cv_creator");
-// define("DB_NAME", "db_cv_generator");
+define("DB_HOST", "localhost");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "db_cv_creator");
